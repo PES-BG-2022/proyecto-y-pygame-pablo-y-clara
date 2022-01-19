acbfa93fd18530441887f1f40a0e362304f0a89d
PabloGutierrez1996 <pablomation@gmail.com>
2022-01-18 21:07:04 -0600

    Definiciones basicas. e importaciones

---
+++ Juego.py
@@ -0,0 +1,14 @@
+from msilib.schema import Font
+import pygame #acceder al comando de pygame
+import random
+import sys
+
+
+pygame.init() #inicializa los módulos de pygame
+#Definiciones
+ANCHO = 800
+ALTO = 600
+color_rojo=(255,0,0)
+color_negro=(0,0,0)
+color_azul = (0,255,0)
+color_texto = (130,130,130)
