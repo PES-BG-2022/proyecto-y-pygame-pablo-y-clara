cbb765058dd9f34220eb2428aaccaf2814659247
claradof95 <ccoradofalla@gmail.com>
2022-01-20 16:25:58 -0600

    Update Juego.py
    Problema de identada, se cambiaron unos nombres porque se confundía.

--- Juego.py
+++ Juego.py
@@ -1,31 +1,32 @@
-import pygame #acceder al comando de pygame
-
+import pygame
 pygame.init()
 
-ANCHO  = 800
-ALTO = 600
-color_rojo=(255,0,0)
-color_negro=(0,0,0)
-color_azul = (0,255,0)
-color_blanco = (130,130,130)
-ancho_juga = 15
-alto_juga = 90
+#Colores
+black = (0, 0, 0)
+rojo = (255,0,0)
+white = (255, 255, 255)
+screen_size = (800, 600)
+player_width = 15
+player_height = 90
 
-consola = pygame.display.set_mode((ANCHO,ALTO)) #consola que proyecta el juego
-reloj = pygame.time.Clock()
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
 
-juga1_x = 50
-juga1_y = 300 - (alto_juga/2)
-juga1_vel = 0
+#Coordenadas y velocidad del jugador 1
+player1_x_coor = 50
+player1_y_coor = 300 - 45
+player1_y_speed = 0
 
-juga2_x = 750 - ancho_juga
-juga2_y = 300 - (alto_juga/2)
-juga2_vel = 0
+#Coordenadas y velocidad del jugador 2
+player2_x_coor = 750 - player_width
+player2_y_coor = 300 - 45
+player2_y_speed = 0
 
-pel_x = 400
-pel_y = 300
-pel_x_vel = 3
-pel_y_vel = 3
+# Coordenadas de la pelota
+pelota_x = 400
+pelota_y = 300
+pelota_speed_x = 3
+pelota_speed_y = 3
 
 game_over = False
 
@@ -35,61 +36,65 @@ while not game_over:
 			game_over = True
 		if event.type == pygame.KEYDOWN:
 			# Jugador 1
-			if event.key == pygame.K_w:
-				juga1_vel = -3
-			if event.key == pygame.K_s:
-				juga1_vel = 3
+			if event.key == pygame.K_w: #sube con la letra w
+				player1_y_speed = -3
+			if event.key == pygame.K_s: #baja con la letra s
+				player1_y_speed = 3
 			# Jugador 2
-			if event.key == pygame.K_UP:
-				juga2_vel = -3
-			if event.key == pygame.K_DOWN:
-				juga2_vel= 3
+			if event.key == pygame.K_UP: #sube con la flecha arriba
+				player2_y_speed = -3
+			if event.key == pygame.K_DOWN: #sube con la flecha abajo
+				player2_y_speed = 3
 
 		if event.type == pygame.KEYUP:
 			# Jugador 1
 			if event.key == pygame.K_w:
-				juga1_vel = 0
+				player1_y_speed = 0
 			if event.key == pygame.K_s:
-				juga1_vel = 0
+				player1_y_speed = 0
 			# Jugador 2
 			if event.key == pygame.K_UP:
-				juga2_vel = 0
+				player2_y_speed = 0
 			if event.key == pygame.K_DOWN:
-				juga2_vel = 0
+				player2_y_speed = 0
 
-	if pel_y > 590 or pel_y < 10:
-		pel_y_vel *= -1
-	if pel_x > 800:
-		pel_x = 400
-		pel_y = 300
+	if pelota_y > 590 or pelota_y < 10: #espacio dentro de la consola en donde se mueve la pelota
+		pelota_speed_y *= -1
+
+	# Revisa si la pelota sale del lado derecho
+	if pelota_x > 800:
+		pelota_x = 400
+		pelota_y = 300
 		# Si sale de la pantalla, invierte direccion
-		pel_x_vel *= -1
-		pel_y_vel *= -1
+		pelota_speed_x *= -1
+		pelota_speed_y *= -1
 
 	# Revisa si la pelota sale del lado izquierdo
-	if pel_x < 0:
-		pel_x = 400
-		pel_y = 300
+	if pelota_x < 0:
+		pelota_x = 400
+		pelota_y = 300
 		# Si sale de la pantalla, invierte direccion
-		pel_x_vel *= -1
-		pel_y_vel *= -1
+		pelota_speed_x *= -1
+		pelota_speed_y *= -1
 
-	juga1_y += juga1_vel
-	juga2_y += juga2_vel
-    # Movimiento pelota
-	pel_x += pel_x_vel
-	pel_y += pel_y_vel
 
-	consola.fill(color_negro)
+	# Modifica las coordenadas para dar mov. a los jugadores/ pelota
+	player1_y_coor += player1_y_speed
+	player2_y_coor += player2_y_speed
+	# Movimiento pelota
+	pelota_x += pelota_speed_x
+	pelota_y += pelota_speed_y
 
-	juga1 = pygame.draw.rect(consola, color_azul, (juga1_x,juga1_y,ancho_juga, alto_juga )) #dibujo paleta izquierda
-	juga2 = pygame.draw.rect(consola, color_azul, (juga2_x,juga1_y,ancho_juga, alto_juga )) #dibujo paleta izquierda
-	pel = pygame.draw.circle(consola, color_rojo, (pel_x, pel_y), 10)
+	screen.fill(black)
+	#Zona de dibujo
+	jugador1 = pygame.draw.rect(screen, white, (player1_x_coor, player1_y_coor, player_width, player_height))
+	jugador2 = pygame.draw.rect(screen, white, (player2_x_coor, player2_y_coor, player_width, player_height))
+	pelota = pygame.draw.circle(screen, rojo, (pelota_x, pelota_y), 10)
 
-	if pel.colliderect(juga1) or pel.colliderect(juga2):
-		pel_x_vel *= -1
+	# Colisiones
+	if pelota.colliderect(jugador1) or pelota.colliderect(jugador2):
+		pelota_speed_x *= -1
 
 	pygame.display.flip()
-	reloj.tick(60)
-
+	clock.tick(60)
 pygame.quit()
